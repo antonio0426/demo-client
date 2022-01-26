a15186767e1c0b1c27e3c2007f6471ec34eb7d76
ant.mangano0426 <ant.mangano@gmail.com>
2022-01-26 18:18:44 +0100

    commit

--- src/main/java/com/client/DemoClientController.java
+++ src/main/java/com/client/DemoClientController.java
@@ -1,36 +1,33 @@
 package com.client;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class DemoClientController {
 
+	private static final Logger log = LoggerFactory.getLogger(DemoClientController.class);
+
 	@GetMapping("/users")
 	public Iterable<DemoDbUser> getAllusers() {
 
-		String baseUrl = "http://demo-db-service/demodb/users";
+		log.info("start getAllUsers");
 
+		String baseUrl = "http://demo-db-service/demodb/users";
 		RestTemplate restTemplate = new RestTemplate();
 		Iterable<DemoDbUser> response = null;
 
 		try {
 			response = restTemplate.getForObject(baseUrl, Iterable.class);
-		} catch (Exception ex) {
-			System.out.println(ex);
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		}
-		System.out.println(response);
+		log.info("end getAllUsers");
 
 		return response;
 	}
