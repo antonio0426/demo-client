0ef547138ab6628c6ada77013bfacfa8975c4826
ant.mangano0426 <ant.mangano@gmail.com>
2022-01-26 12:06:17 +0100

    commit

--- src/main/java/com/client/DemoClientController.java
+++ src/main/java/com/client/DemoClientController.java
@@ -20,7 +20,7 @@ public class DemoClientController {
 	@GetMapping("/users")
 	public Iterable<DemoDbUser> getAllusers() {
 
-		String baseUrl = "http://demo-client-service/demodb/users";
+		String baseUrl = "http://demo-db-service/demodb/users";
 
 		RestTemplate restTemplate = new RestTemplate();
 		Iterable<DemoDbUser> response = null;
