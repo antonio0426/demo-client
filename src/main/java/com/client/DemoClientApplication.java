42f8cc69a5f2c3fc40a1354793aa324ee14ee4c3
ant.mangano0426 <ant.mangano@gmail.com>
2022-01-27 19:13:07 +0100

    commit

--- src/main/java/com/client/DemoClientApplication.java
+++ src/main/java/com/client/DemoClientApplication.java
@@ -6,17 +6,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-
 @SpringBootApplication
 @EnableDiscoveryClient
-public class DemoClientApplication { 
+public class DemoClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoClientApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 }
